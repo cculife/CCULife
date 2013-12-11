2684a4be27521e91cba9f634c7ecaf7385fe1c0f
Zankio <xxoojoeooxx1@gmail.com>
2013-12-11 16:41:16 +0800

    Change implements to BaseSource

--- CCULife/src/main/java/org/zankio/cculife/CCUService/Source/KikiSource.java
+++ CCULife/src/main/java/org/zankio/cculife/CCUService/Source/KikiSource.java
@@ -3,7 +3,7 @@ package org.zankio.cculife.CCUService.Source;
 import org.zankio.cculife.CCUService.Kiki;
 import org.zankio.cculife.SessionManager;
 
-public abstract class KikiSource implements ISource{
+public abstract class KikiSource extends BaseSource {
     public abstract boolean Authentication(SessionManager sessionManager) throws Exception;
     public abstract Kiki.TimeTable getTimeTable() throws Exception;
     public abstract Kiki.Course[] getCourseList(int year, int term) throws Exception;
--- CCULife/src/main/java/org/zankio/cculife/CCUService/Source/ScoreQuerySource.java
+++ CCULife/src/main/java/org/zankio/cculife/CCUService/Source/ScoreQuerySource.java
@@ -3,7 +3,7 @@ package org.zankio.cculife.CCUService.Source;
 import org.zankio.cculife.CCUService.ScoreQuery;
 import org.zankio.cculife.SessionManager;
 
-public abstract class ScoreQuerySource implements ISource{
+public abstract class ScoreQuerySource extends BaseSource {
     public abstract boolean Authentication(SessionManager sessionManager) throws Exception;
     public abstract ScoreQuery.Grade[] getGrades() throws Exception;
 }
