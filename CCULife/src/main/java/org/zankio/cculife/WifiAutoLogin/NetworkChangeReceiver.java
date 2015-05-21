aecf70d7883e6ec9de5e3096893db9879c797dc5
DanSnow <dododavid006@gmail.com>
2015-05-21 16:58:09 +0800

    Add check for pref 'autologin_enable'

--- CCULife/src/main/java/org/zankio/cculife/WifiAutoLogin/NetworkChangeReceiver.java
+++ CCULife/src/main/java/org/zankio/cculife/WifiAutoLogin/NetworkChangeReceiver.java
@@ -8,9 +8,12 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 import android.net.wifi.WifiManager;
 import android.net.NetworkInfo;
+
+import android.preference.PreferenceManager;
 
 import org.zankio.cculife.R;
 import org.zankio.cculife.Debug;
@@ -21,6 +24,11 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
   @TargetApi(11)
   public void onReceive(final Context context, final Intent intent) {
     String action = intent.getAction();
+    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+    Debug debug = new Debug();
+    if(!prefs.getBoolean("autologin_enable", false)) {
+      return;
+    }
     WifiAccount account = WifiAccount.getInstance(context);
     if(action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
       WifiManager manager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
@@ -28,7 +36,6 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
       NetworkInfo.State state = networkInfo.getState();
 
       if(state == NetworkInfo.State.CONNECTED) { // Network is connect
-        Debug debug = new Debug();
         String connectingSSID = manager.getConnectionInfo().getSSID().replace("\"", "");
         if(connectingSSID.equals("CCU")) {
           debug.info("Start service");
