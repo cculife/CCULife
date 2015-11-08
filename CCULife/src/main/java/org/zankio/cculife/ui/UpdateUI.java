cdbd1c6b202431c0b368edae8d452c885e992a89
Zankio <xxoojoeooxx1@gmail.com>
2015-11-09 06:28:56 +0800

    add check update service

---
+++ CCULife/src/main/java/org/zankio/cculife/ui/UpdateUI.java
@@ -0,0 +1,24 @@
+package org.zankio.cculife.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+
+import org.zankio.cculife.ui.dialog.UpdateDialog;
+
+public class UpdateUI extends FragmentActivity{
+    private final static String TAG = "UpdataUI";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        Bundle data = new Bundle();
+        data.putString("version", intent.getStringExtra("version"));
+        data.putString("description", intent.getStringExtra("description"));
+        DialogFragment dialog = new UpdateDialog();
+        dialog.setArguments(data);
+        dialog.show(getSupportFragmentManager(), TAG);
+
+    }
+}
