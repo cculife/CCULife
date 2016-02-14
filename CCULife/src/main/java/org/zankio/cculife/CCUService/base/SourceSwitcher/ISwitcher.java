359cfdd4b39c2345dfb9f11a195217a0317ac0c5
Zankio <xxoojoeooxx1@gmail.com>
2016-02-14 16:12:39 +0800

    remove source switcher

--- CCULife/src/main/java/org/zankio/cculife/CCUService/base/SourceSwitcher/ISwitcher.java
+++
@@ -1,9 +0,0 @@
-package org.zankio.cculife.CCUService.base.SourceSwitcher;
-
-import org.zankio.cculife.CCUService.base.source.ISource;
-
-public interface ISwitcher {
-    public ISource getSource();
-    public void closeSource();
-    public void openSource();
-}
