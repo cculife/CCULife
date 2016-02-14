564887883c22e82314669138831299de667393dd
Zankio <xxoojoeooxx1@gmail.com>
2016-02-14 16:28:28 +0800

    remove useless class

--- CCULife/src/main/java/org/zankio/cculife/ui/Base/onDataLoadListener.java
+++
@@ -1,5 +0,0 @@
-package org.zankio.cculife.ui.Base;
-
-public interface onDataLoadListener<T> {
-    public void onDataLoaded(T data);
-}
--- CCULife/src/main/java/org/zankio/cculife/ui/Base/onErrorListener.java
+++
@@ -1,5 +0,0 @@
-package org.zankio.cculife.ui.Base;
-
-public interface onErrorListener {
-    public void onError(String msg);
-}
