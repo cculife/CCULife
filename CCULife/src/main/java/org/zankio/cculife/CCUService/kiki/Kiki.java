4a7f0c9ff262dda1ef8b66d3c1f29d43081ea3e7
Zankio <xxoojoeooxx1@gmail.com>
2016-02-14 16:22:02 +0800

    new kiki

--- CCULife/src/main/java/org/zankio/cculife/CCUService/kiki/Kiki.java
+++ CCULife/src/main/java/org/zankio/cculife/CCUService/kiki/Kiki.java
@@ -1,172 +1,83 @@
 package org.zankio.cculife.CCUService.kiki;
 
+
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.preference.PreferenceManager;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.zankio.cculife.CCUService.base.SourceSwitcher.AutoNetworkSourceSwitcher;
-import org.zankio.cculife.CCUService.base.SourceSwitcher.ISwitcher;
-import org.zankio.cculife.CCUService.base.SourceSwitcher.SingleSourceSwitcher;
-import org.zankio.cculife.CCUService.ecourse.Ecourse;
-import org.zankio.cculife.CCUService.kiki.parser.KikiParser;
-import org.zankio.cculife.CCUService.kiki.source.KikiLocalSource;
-import org.zankio.cculife.CCUService.kiki.source.KikiRemoteSource;
-import org.zankio.cculife.CCUService.kiki.source.KikiSource;
-import org.zankio.cculife.SessionManager;
-import org.zankio.cculife.override.Net;
-import org.zankio.cculife.override.NetworkErrorException;
+import org.zankio.cculife.CCUService.base.BaseRepo;
+import org.zankio.cculife.CCUService.base.BaseSession;
+import org.zankio.cculife.CCUService.base.authentication.QueryStringAuth;
+import org.zankio.cculife.CCUService.base.source.BaseSource;
+import org.zankio.cculife.CCUService.kiki.source.local.DatabaseTimeTableSource;
+import org.zankio.cculife.CCUService.kiki.source.remote.Authenticate;
+import org.zankio.cculife.CCUService.kiki.source.remote.CourseListSource;
+import org.zankio.cculife.CCUService.kiki.source.remote.TimetableSource;
+import org.zankio.cculife.UserManager;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Comparator;
+public class Kiki extends BaseRepo<String> {
+    public enum OfflineMode {
+        ALL,
+        CLASS,
+        BROWSERED,
+        DISABLED
+    }
+    private OfflineMode offline_mode;
+    private UserManager userManager;
 
-import javax.net.ssl.HttpsURLConnection;
+    public Kiki(Context context) {
+        super(context);
+        setSession(new BaseSession<String>(new QueryStringAuth()));
+    }
 
-public class Kiki {
+    @Override
+    protected void initialSource() {
+        loadPreferences();
+        super.initialSource();
+    }
 
-    public ISwitcher sourceSwitcher;
-    public int OFFLINE_MODE = -1;
-
-    public Kiki(Context context) throws Exception {
-        KikiLocalSource kikiLocalSource;
-        KikiRemoteSource kikiRemoteSource;
-        SessionManager sessionManager;
-        SharedPreferences preferences;
-
-        sessionManager = SessionManager.getInstance(context);
-        preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        OFFLINE_MODE = sessionManager.isSave() &&
-                preferences.getBoolean("offline_enable", true)
-                ? 1 : 0;
-
-        kikiRemoteSource = new KikiRemoteSource(this, new KikiParser());
-
-        try {
-            kikiRemoteSource.Authenticate(sessionManager);
-        } catch (NetworkErrorException e) { }
-        HttpsURLConnection.setDefaultSSLSocketFactory(org.zankio.cculife.CCUService.base.helper.ConnectionHelper.getSSLSocketFactory());
-
-        if (OFFLINE_MODE > 0) {
-            kikiLocalSource = new KikiLocalSource(this, context);
-            kikiRemoteSource.setLocalSource(kikiLocalSource);
-            sourceSwitcher = new AutoNetworkSourceSwitcher(context, kikiLocalSource, kikiRemoteSource);
+    private void loadPreferences() {
+        this.userManager = UserManager.getInstance(getContext());
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        if (userManager.isSave() && preferences.getBoolean("offline_enable", true)) {
+            int offline = Integer.valueOf(preferences.getString("offline_mode", "1"));
+            offline_mode = OfflineMode.values()[offline];
         } else {
-            sourceSwitcher = new SingleSourceSwitcher(kikiRemoteSource);
-        }
-
-    }
-
-    public KikiSource getSource() {
-        return (KikiSource) sourceSwitcher.getSource();
-    }
-
-    public TimeTable getTimeTable() throws Exception {
-        return getSource().getTimeTable();
-    }
-
-    //ToDo Day-Class to Class-Day
-
-    public class TimeTable {
-
-        public Day[] days;
-
-        public TimeTable() {
-            days = new Day[7];
-            for (int i = 0; i < 7; i++) {
-                days[i] = new Day();
-            }
-        }
-
-        public class Day {
-            public ArrayList<Class> classList;
-
-            public Day() {
-                classList = new ArrayList<Class>();
-            }
-        }
-
-        public class Class {
-            public String name;
-            public String classroom;
-            public String teacher;
-            public Calendar start;
-            public Calendar end;
-            public int color;
-            public int colorid;
-        }
-
-        public void sort() {
-            Comparator<Class> comparator = new Comparator<Class>() {
-                public int compare(Class a, Class b) {
-                    return a.start.compareTo(b.start);
-                }
-            };
-
-            for (int i = 0; i < 7; i++) {
-                Collections.sort(days[i].classList, comparator);
-            }
+            offline_mode = OfflineMode.DISABLED;
         }
     }
 
-    public Ecourse getEcourseCourse(int year, int term, Ecourse ecourse) throws Exception {
-        Course[] courses = getCourseList(year, term);
+    @Override
+    protected boolean filterSource(BaseSource source) {
+        return offline_mode.compareTo(OfflineMode.DISABLED) != 0 || !source.property.isOffline;
 
-        return null;
     }
 
-    public static class Course {
-        public String CourseID;
-        public String ClassID;
-        public String Name;
-        public String Teacher;
-        public int Credit;
-        public String CreditType;
-        public Time[] Time;
-        public String ClassRoom;
-        public String OutlineLink;
-        public int term;
-        public int year;
+    @Override
+    protected BaseSource[] getSources() {
+        return new BaseSource[]{
+                new CourseListSource(this),
+                new TimetableSource(this),
+                new Authenticate(this),
 
-        public String getEcourseID() {
-            if ("".equals(OutlineLink)) return null;
-            if (year > 0 && term > 0)
-                OutlineLink = OutlineLink
-                        .replaceAll("([\\?&])year=\\d+", "$1year=" + year)
-                        .replaceAll("([\\?&])term=\\d+", "$1term=" + term);
-
-            Connection connection;
-            String location, result;
-
-            connection = Jsoup.connect(OutlineLink).timeout(Net.CONNECT_TIMEOUT);
-            connection.followRedirects(false);
-
-            try {
-                location = connection.execute().header("Location");
-                if (location != null) {
-                    result = Uri.parse(location).getQueryParameter("courseid");
-                    if (result != null)
-                        return String.format("%d_%d_%s", year, term, result);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-        public static class Time {
-            public static Time prase(String data) {
-                return null;
-            }
-        }
+                new DatabaseTimeTableSource(this),
+        };
     }
-    public Course[] getCourseList() throws Exception {
-        return getCourseList(-1, -1);
+
+    public OfflineMode getOfflineMode() {
+        return offline_mode;
     }
-    public Course[] getCourseList(int year, int term) throws Exception {
-        return getSource().getCourseList(year, term);
+
+    public UserManager getUserManager() {
+        return this.userManager;
     }
+
+    public String getUsername() {
+        return this.userManager.getUserName();
+    }
+
+    public String getPassword() {
+        return this.userManager.getPassword();
+    }
+
 }
