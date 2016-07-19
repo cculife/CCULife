539a970b4b35593106d31e154e73c3b2087d2bbf
Zankio <xxoojoeooxx1@gmail.com>
2016-07-19 10:54:40 +0800

    move out bus and train to ccudata

--- CCULife/src/main/java/org/zankio/cculife/CCUService/bus/model/BusStop.java
+++
@@ -1,9 +0,0 @@
-package org.zankio.cculife.CCUService.bus.model;
-
-public class BusStop {
-    public String id;
-    public String name;
-    public String seq;
-    public String perdiction;
-    public String carNo;
-}
