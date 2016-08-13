16496f0bb87147ab88ca27061cad3231f1fb010f
Zankio <xxoojoeooxx1@gmail.com>
2016-08-13 21:17:10 +0800

    remove AsyncTask

--- CCULife/src/main/java/org/zankio/cculife/ui/CCUSchedule/CCUScheduleActivity.java
+++ CCULife/src/main/java/org/zankio/cculife/ui/CCUSchedule/CCUScheduleActivity.java
@@ -18,6 +18,10 @@ import org.zankio.cculife.ui.base.BaseActivity;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 public class CCUScheduleActivity extends BaseActivity {
 
     private ScheduleAdapter adapter;
@@ -41,7 +45,48 @@ public class CCUScheduleActivity extends BaseActivity {
         TODAY_DAY_OF_YEAR = today.get(Calendar.DAY_OF_YEAR);
         TODAY_YEAR = today.get(Calendar.YEAR);
 
-        new LoadDataAsyncTask().execute();
+        loadSchedule();
+    }
+
+    private void loadSchedule() {
+        CCUSchedule schedule = new CCUSchedule(CCUScheduleActivity.this);
+
+        Observable.just(schedule)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(CCUSchedule::getScheduleList)
+                .subscribe(result -> {
+
+                    // check no result
+                    if (result == null || result.length == 0) {
+                        message().show("沒有日程");
+                        return;
+                    }
+
+
+                    int i, length = 0;
+                    for (i = 0; i < result.length; i++) {
+                        length += result[i].list.length;
+                    }
+                    CCUSchedule.Item[] list = new CCUSchedule.Item[length + result.length];
+
+                    i = 0;
+                    for (CCUSchedule.Schedule s : result) {
+                        CCUSchedule.Item header = schedule.new Item();
+                        header.Title = s.Name;
+                        header.Date = s.list[0].Date;
+                        list[i++] = header;
+
+                        for (CCUSchedule.Item item : s.list) {
+                            list[i++] = item;
+                        }
+                    }
+
+                    adapter.setItems(list);
+                    message().hide();
+
+                    scrollToNow(list);
+                });
     }
 
     @Override
@@ -54,54 +99,6 @@ public class CCUScheduleActivity extends BaseActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         last_scroll_item = savedInstanceState.getInt("scroll_item", -1);
-    }
-
-    public class LoadDataAsyncTask extends AsyncTaskWithErrorHanding<Void, Void, CCUSchedule.Schedule[]> {
-
-        private CCUSchedule schedule = null;
-
-        @Override
-        protected void onError(Exception e, String msg) {
-            message().show(msg);
-        }
-
-        @Override
-        protected void _onPostExecute(CCUSchedule.Schedule[] result) {
-            if (result == null || result.length == 0) {
-                message().show("沒有日程");
-                return;
-            }
-
-
-            int i, length = 0;
-            for (i = 0; i < result.length; i++) {
-                length += result[i].list.length;
-            }
-            CCUSchedule.Item[] list = new CCUSchedule.Item[length + result.length];
-
-            i = 0;
-            for (CCUSchedule.Schedule s : result) {
-                CCUSchedule.Item header = schedule.new Item();
-                header.Title = s.Name;
-                header.Date = s.list[0].Date;
-                list[i++] = header;
-
-                for (CCUSchedule.Item item : s.list) {
-                    list[i++] = item;
-                }
-            }
-
-            adapter.setItems(list);
-            message().hide();
-
-            scrollToNow(list);
-        }
-
-        @Override
-        protected CCUSchedule.Schedule[] _doInBackground(Void... params) throws Exception {
-            schedule = new CCUSchedule(CCUScheduleActivity.this);
-            return schedule.getScheduleList();
-        }
     }
 
     @Override
