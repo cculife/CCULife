a2fcab889460b0459cfdba40747f522816290f53
Zankio <xxoojoeooxx1@gmail.com>
2016-08-27 22:46:48 +0800

    lint: 'static' method declared 'final'

--- CCULife/src/main/java/org/zankio/cculife/utils/UnitUtils.java
+++ CCULife/src/main/java/org/zankio/cculife/utils/UnitUtils.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.util.TypedValue;
 
 public class UnitUtils {
-    public static final int getDp(Context context, int px) {
+    public static int getDp(Context context, int px) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, context.getResources().getDisplayMetrics());
     }
 }
