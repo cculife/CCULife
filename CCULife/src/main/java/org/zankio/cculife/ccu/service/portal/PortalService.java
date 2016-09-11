60b1affe9663c0c210b6dfe75b571941503ddc39
Zankio <xxoojoeooxx1@gmail.com>
2016-09-12 00:42:12 +0800

    add portal error cache and message

--- CCULife/src/main/java/org/zankio/cculife/ccu/service/portal/PortalService.java
+++ CCULife/src/main/java/org/zankio/cculife/ccu/service/portal/PortalService.java
@@ -1,6 +1,7 @@
 package org.zankio.cculife.ccu.service.portal;
 
 import android.app.Activity;
+import android.widget.Toast;
 
 import org.zankio.ccudata.portal.Portal;
 import org.zankio.ccudata.portal.model.PortalData;
@@ -12,20 +13,32 @@ import rx.Observable;
 
 public class PortalService {
     public static void openPortal(Activity activity, Portal portal, PortalData data) {
+        Toast.makeText(activity, "請稍候...", Toast.LENGTH_SHORT).show();
+
         portal
                 .fetch(SSOLoginSource.request(data))
-                .subscribe(response -> {
-                    String url = response.data();
+                .subscribe(
+                        response -> {
+                            String url = response.data();
 
-                    Observable.just(url)
-                            .compose(data.after())
-                            .toList()
-                            .subscribe(strings ->
-                                    BrowserUtils.open(
-                                            activity,
-                                            strings.toArray(new String[strings.size()])
-                                    )
-                            );
-                });
+                            Observable.just(url)
+                                    .compose(data.after())
+                                    .toList()
+                                    .subscribe(strings ->
+                                                    BrowserUtils.open(
+                                                            activity,
+                                                            strings.toArray(new String[strings.size()])
+                                                    ),
+                                            throwable -> {
+                                                Toast.makeText(activity, "使用瀏覽器開啟錯誤", Toast.LENGTH_SHORT).show();
+                                                throwable.printStackTrace();
+                                            }
+                                    );
+                        },
+                        throwable -> {
+                            Toast.makeText(activity, "使用瀏覽器開啟錯誤", Toast.LENGTH_SHORT).show();
+                            throwable.printStackTrace();
+                        }
+                );
     }
 }
