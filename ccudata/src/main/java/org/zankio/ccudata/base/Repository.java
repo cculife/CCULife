3bb76c368809aa3075149c53e407fbe62e3aebd8
Zankio <xxoojoeooxx1@gmail.com>
2016-09-12 00:44:06 +0800

    add repo timeout

--- ccudata/src/main/java/org/zankio/ccudata/base/Repository.java
+++ ccudata/src/main/java/org/zankio/ccudata/base/Repository.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
@@ -71,6 +72,7 @@ public abstract class Repository {
                 .compose(filterResult())
                 .compose(bindListener(request.type))
                 .doOnError(Throwable::printStackTrace)
+                .timeout(60, TimeUnit.SECONDS)
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
