c9646b36f373129688b897b258d5993fd41de8ac
Zankio <xxoojoeooxx1@gmail.com>
2017-09-23 19:26:09 +0800

    add fabric

--- CCULife/src/main/java/org/zankio/cculife/CCULifeApplication.java
+++ CCULife/src/main/java/org/zankio/cculife/CCULifeApplication.java
@@ -2,8 +2,10 @@ package org.zankio.cculife;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.squareup.leakcanary.LeakCanary;
 
+import io.fabric.sdk.android.Fabric;
 import org.zankio.ccudata.base.source.http.HTTPSource;
 import org.zankio.cculife.override.Net;
 
@@ -14,6 +16,7 @@ public class CCULifeApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         if (LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
             // You should not init your app in this process.
